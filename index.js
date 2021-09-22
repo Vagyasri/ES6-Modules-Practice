1d2df59bda22333bc40faa7ac7af08dabb077823
Lucky <vagyasri2011@gmail.com>
2021-09-22 08:45:06 +0200

    Call the class from app.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+/* eslint-disable no-console */
 import Bio from './app.js';
+
+const user1 = new Bio('Lucky', 28);
+console.log(user1);
+
+// CAN BE DONE LIKE BELOW as import can change the name of an object
+// import B from './app.js';
+// const user1 = new B('Lucky', 28);
+// console.log(user1);
+
