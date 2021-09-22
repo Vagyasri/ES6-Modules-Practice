b838d0aba93c9fd53f58afa502e9dc30abd58d80
Lucky <vagyasri2011@gmail.com>
2021-09-22 08:50:00 +0200

    Import class along with functions

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 /* eslint-disable no-console */
-import Bio from './app.js';
+// * Import class
+// import Bio from './app.js';
 
-const user1 = new Bio('Lucky', 28);
-console.log(user1);
+import Bio from "./app"
 
-// CAN BE DONE LIKE BELOW as import can change the name of an object
+// const user1 = new Bio('Lucky', 28);
+// console.log(user1);
+
+// * CAN BE DONE LIKE BELOW as import can change the name of an object
 // import B from './app.js';
 // const user1 = new B('Lucky', 28);
 // console.log(user1);
+
+// Import class with functions
+import Bio, {displayName, displayAge} from './app.js'
 
